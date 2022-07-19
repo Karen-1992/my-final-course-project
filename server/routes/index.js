b953b47a447e524e4e9139add1b3438c9a33292b
Karen Habuzian <karen.gabuzyan@gmail.com>
2022-07-20 00:53:50 +0300

    server: routes, auth, tokens

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+router.use("/auth", require("./auth.routes"));
+router.use("/comment", require("./comment.routes"));
+router.use("/user", require("./user.routes"));
+router.use("/category", require("./category.routes"));
+
+module.exports = router;
